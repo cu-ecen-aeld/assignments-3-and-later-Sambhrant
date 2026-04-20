dae9bf86369aa10bcb87caf0c2100487dc06d97d
Sambhrant Chaubey <i.sambhrant.online@gmail.com>
2026-04-20 19:51:31 +0530

    Implement system call handling and process management in systemcalls.c; update header file and remove redundant build commands from finder-test.sh; add testfile.txt

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,6 +16,10 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int ret = system(cmd);
+    if (ret == -1) {
+        return false;
+    }
 
     return true;
 }
@@ -58,6 +62,24 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    pid_t pid = fork();
+    if (pid < 0) {
+        return false;
+    } else if (pid == 0) {
+        execv(command[0], command);
+        // If execv returns, it must have failed.
+        exit(1);
+    } else {
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            return false;
+        }
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }  
 
     va_end(args);
 
@@ -92,6 +114,30 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    pid_t pid = fork();
+    if (pid < 0) {
+        return false;
+    } else if (pid == 0) {
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd < 0) {
+            exit(1);
+        }
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+        execv(command[0], command);
+        // If execv returns, it must have failed.
+        exit(1);
+    } else {
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            return false;
+        }
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     va_end(args);
 
